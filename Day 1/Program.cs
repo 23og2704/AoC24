3e12e634830355338748e471fa8869b57578b154
wxterfall <23og2704@barton.ac.uk>
2024-11-28 14:27:26 +0000

    Add project files.

---
+++ Day 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Day_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            int answer = 0;
+
+
+
+            Console.WriteLine(answer.ToString());
+            Console.ReadKey();
+        }
+    }
+}
