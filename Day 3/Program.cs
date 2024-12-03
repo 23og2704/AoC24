a588840bc56ff90acd4db6935e06f220b8a76671
wxterfall <23og2704@barton.ac.uk>
2024-12-03 12:17:30 +0000

    Day 3 complete, i hate regex

--- Day 3/Program.cs
+++ Day 3/Program.cs
@@ -1,35 +1,44 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Day_3
 {
     internal class Program
     {
-        static int part1()
+        static int day3()
         {
-            string[] lines = File.ReadAllLines("input.txt");
+            string text = File.ReadAllText("input.txt");
             int answer = 0;
+            string regEx = @"(?<do>do\(\))|(?<dont>don't\(\))|mul\((?<x>\d{1,3}),(?<y>\d{1,3})\)";
 
+            bool enabled = true;
+            var matches = Regex.Matches(text, regEx);
 
+            foreach (Match m in matches)
+            {
+                if (m.Groups["do"].Success)
+                {
+                    enabled = true;
+                }
+                else if (m.Groups["dont"].Success)
+                {
+                    enabled = false;
+                }
+                if (m.Groups["x"].Success && m.Groups["y"].Success && enabled)
+                {
+                    int x = int.Parse(m.Groups["x"].Value);
+                    int y = int.Parse(m.Groups["y"].Value);
+                    answer += x * y;
+
+                }
+            }
             return answer;
         }
 
-        static int part2()
-        {
-            string[] lines = File.ReadAllLines("input.txt");
-            int answer = 0;
-
-
-            return answer;
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine($"Part 1: {part1()}");
-            Console.WriteLine($"Part 2: {part2()}");
+            Console.WriteLine($"Day 3: {day3()}");
             Console.ReadKey();
         }
     }
--- Day 5/Program.cs
+++ Day 5/Program.cs
@@ -4,9 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 
-namespace Day_4
+namespace Day_5
 {
     internal class Program
     {
