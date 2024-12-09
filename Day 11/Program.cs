93a496400b759379c9907382b2b7a7470a1adc49
wxterfall <23og2704@barton.ac.uk>
2024-12-09 15:21:04 +0000

    d

---
+++ Day 11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_11
+{
+    internal class Program
+    {
+        static int part1()
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            int answer = 0;
+
+
+            return answer;
+        }
+
+        static int part2()
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            int answer = 0;
+
+
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Part 1: {part1()}");
+            Console.WriteLine($"Part 2: {part2()}");
+            Console.ReadKey();
+        }
+    }
+}
