dba292d50568fb937880c818cdb16069a226924e
wxterfall <23og2704@barton.ac.uk>
2024-12-11 19:50:23 +0000

    completed day 11

--- Day 11/Program.cs
+++ Day 11/Program.cs
@@ -4,28 +4,132 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Security.Policy;
 
 namespace Day_11
 {
     internal class Program
     {
-        static int part1()
+        static long part1()
         {
-            string[] lines = File.ReadAllLines("input.txt");
-            int answer = 0;
+            string lines = File.ReadAllText("input.txt");
+
+            string[] stones = lines.Split(' ');
+            List<long> startStones = new List<long>();
+
+            
+
+            foreach (string s in stones)
+            {
+                startStones.Add(long.Parse(s)); 
+            }
+            for (int i = 0; i < 75; i++)
+            {
+                List<long> newStones = new List<long>();
+                foreach (var s in startStones)
+                {
+                    newStones.AddRange(blink(s));
+                }
+                startStones = newStones;
+
+            }
 
 
-            return answer;
+
+            return startStones.Count;
+        }
+        static List<long> blink(long stone)
+        {
+            List<long> newStones = new List<long>();
+
+            if (stone == 0)
+            {
+                newStones.Add(1);
+            } else
+            {
+                if (stone.ToString().Length % 2 == 0)
+                {
+                    string leftStone  = stone.ToString().Substring(0, stone.ToString().Length / 2);
+                    string rightStone = stone.ToString().Substring((stone.ToString().Length / 2));
+
+                    newStones.Add(long.Parse(leftStone)); 
+                    newStones.Add(long.Parse(rightStone));
+                }
+                else
+                {
+                    newStones.Add(stone * 2024);
+                }
+            }
+            return newStones;
+
+        }
+        static long part2()
+        {
+            string lines = File.ReadAllText("input.txt");
+            string[] stones = lines.Split(' ');
+            Dictionary<long, long> stoneCount = new Dictionary<long, long>();
+
+            foreach (string s in stones)
+            {
+                if (stoneCount.ContainsKey(long.Parse(s)))
+                    stoneCount[long.Parse(s)]++;
+                else
+                    stoneCount[long.Parse(s)] = 1;
+            }
+
+            for (int i = 0; i < 75; i++)
+            {
+                Dictionary<long, long> newStoneCount = new Dictionary<long, long>();
+
+                foreach (var kvp in stoneCount)
+                {
+                    long stone = kvp.Key;
+                    long count = kvp.Value;
+
+                    if (stone == 0)
+                    {
+                        if (newStoneCount.ContainsKey(1))
+                            newStoneCount[1] += count;
+                        else
+                            newStoneCount[1] = count;
+                    }
+                    else
+                    {
+                        string stoneStr = stone.ToString();
+                        if (stoneStr.Length % 2 == 0)
+                        {
+                            string leftStoneStr = stoneStr.Substring(0, stoneStr.Length / 2);
+                            string rightStoneStr = stoneStr.Substring(stoneStr.Length / 2);
+                            long leftStone = long.Parse(leftStoneStr);
+                            long rightStone = long.Parse(rightStoneStr);
+
+                            if (newStoneCount.ContainsKey(leftStone))
+                                newStoneCount[leftStone] += count;
+                            else
+                                newStoneCount[leftStone] = count;
+
+                            if (newStoneCount.ContainsKey(rightStone))
+                                newStoneCount[rightStone] += count;
+                            else
+                                newStoneCount[rightStone] = count;
+                        }
+                        else
+                        {
+                            long newStone = stone * 2024;
+                            if (newStoneCount.ContainsKey(newStone))
+                                newStoneCount[newStone] += count;
+                            else
+                                newStoneCount[newStone] = count;
+                        }
+                    }
+                }
+                stoneCount = newStoneCount;
+            }
+            return stoneCount.Values.Sum();
         }
 
-        static int part2()
-        {
-            string[] lines = File.ReadAllLines("input.txt");
-            int answer = 0;
 
-
-            return answer;
-        }
         static void Main(string[] args)
         {
             Console.WriteLine($"Part 1: {part1()}");
